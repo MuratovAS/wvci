3b0e5be709183e717d552d0020061222621c3f56
MuratovAS <lexamzx@gmail.com>
2025-04-23 14:50:59 +0200

    Update wvMake.sh

--- scripts/wvMake.sh
+++ scripts/wvMake.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+git config --global --add safe.directory '*'
+
 OUTPUT_DIR=${OUTPUT_DIR:-"build"}
 PRJ_VERSION=${PRJ_VERSION:-"git"}
 
-if [ "$@" != "" ]; then
+if [[ ! -z "$@" ]]; then
 	FILES="$@"; 
 else
-	FILES=$( git diff --name-only HEAD~1 | grep '\.yml' | tr '\n' ' ');
+	FILES=$( git diff --name-only HEAD~1 | grep '\.yml' | grep -v ".git" | tr '\n' ' ');
 fi
 
 for arg in $FILES
@@ -24,10 +26,10 @@ do
 	mkdir -p $OUTPUT_DIR
 	
 	if [ "$PRJ_VERSION" == "git" ]; then
-		PRJ_VERSION="hash:`git log --date=format:'%Y%m%d%H' --pretty=tformat:"%h [%cd]" $FILE | head -n1`"
+		PRJ_VERSION="hash:`git log --date=format:'%Y%m%d%H' --pretty=tformat:"%h-[%cd]" $FILE | head -n1`"
 	fi
 	
 	sed -i "s/vV.V.V-VVV/\"$PRJ_VERSION\"/g" $FILE
 	wireviz -f hp -o $OUTPUT_DIR $FILE; 
-	sed -i "s/\"$PRJ_VERSION\"/vV.V.V-VVV/g" $FILE
+	git restore $FILE
 done
